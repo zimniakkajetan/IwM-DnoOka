4b54fb012df08e0bf3e94aa4e9330b14ce579c8c
wblachowski <wojciech@blachowski.pl>
2018-04-14 14:51:03 +0200

    Dodanie 2 metod domykających naczynia, proste odszumianie

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import PIL
 from skimage import io, filters
 from skimage.color import rgb2gray
 from PIL import ImageFilter
-
+import cv2
 
 
 class obraz():
@@ -35,20 +35,22 @@ class Window(Frame):
         self.pack(fill=BOTH, expand=1)
         self.grid(padx=4,pady=4)
 
+        canvasSize=300
+
         # creating image canvases
-        self.inputCanvas = Canvas(self,width=200, height=200,bg='white')
-        self.inputCanvas.create_rectangle(2,2,200,200)
-        self.inputCanvas.create_text(100,100,text="Obraz wejściowy")
+        self.inputCanvas = Canvas(self,width=canvasSize, height=canvasSize,bg='white')
+        self.inputCanvas.create_rectangle(2,2,canvasSize,canvasSize)
+        self.inputCanvas.create_text(canvasSize/2,canvasSize/2,text="Obraz wejściowy")
         self.inputCanvas.grid(row=0,column=0)
 
-        self.firstCanvas = Canvas(self, width=200,height=200,bg='white')
-        self.firstCanvas.create_rectangle(2,2,200,200)
-        self.firstCanvas.create_text(100,100,text="Obraz po pierwszej obróbce")
+        self.firstCanvas = Canvas(self, width=canvasSize,height=canvasSize,bg='white')
+        self.firstCanvas.create_rectangle(2,2,canvasSize,canvasSize)
+        self.firstCanvas.create_text(canvasSize/2,canvasSize/2,text="Obraz po pierwszej obróbce")
         self.firstCanvas.grid(row=0,column=1)
 
-        self.outputCanvas = Canvas(self,width=200,height=200,bg='white')
-        self.outputCanvas.create_rectangle(2,2,200,200)
-        self.outputCanvas.create_text(100,100,text="Obraz wyjściowy")
+        self.outputCanvas = Canvas(self,width=canvasSize,height=canvasSize,bg='white')
+        self.outputCanvas.create_rectangle(2,2,canvasSize,canvasSize)
+        self.outputCanvas.create_text(canvasSize/2,canvasSize/2,text="Obraz wyjściowy")
         self.outputCanvas.grid(row=0,column=2)
 
         self.uploadInputButton = Button(self,text="Wgraj obraz",command=self.upload_input_file)
@@ -74,7 +76,6 @@ class Window(Frame):
     def set_image(self,path,canvas):
         img = Image.open(path)
         print(img)
-        temp = np.array(img.convert('L'))
 
         img = img.resize((canvas.winfo_width(), canvas.winfo_height()), Image.ANTIALIAS)
 
@@ -94,18 +95,39 @@ class Window(Frame):
     def firstStep1(self):
         pic = obraz.wejsciowy
         print(pic)
+        pic = pic.convert('L')
 
-        pic2 = np.array(pic)
-        pic2[:, :, 0] = 0
-        pic2[:, :, 2] = 0
-        pic = Image.fromarray(pic2)
         pic = pic.filter(ImageFilter.FIND_EDGES)
-        print(pic)
-        self.setFirstStepOutput(pic)
 
-        #obraz.sinogram = np.array(sinogram)
-        #start_new_thread(self.secondStep, (pic))
+        pic = np.array(pic)
+
+        th, pic = cv2.threshold(pic, 20, 255, cv2.THRESH_BINARY);
+
+        #filtr gaszący pixel jeżeli każdy z 8 sąsiadów jest zgaszony
+        pic=self.denoise(pic)
+        #zamkniecie naczyn
+        pic=self.contourClose(pic)
+        #alternatywne:
+        #pic=self.morphologicClose(pic)
+
+        self.setFirstStepOutput(Image.fromarray(pic))
         return pic
+
+    def denoise(self,pic):
+        for i in range(1,pic.shape[0]-1):
+            for j in range(1, pic.shape[1]-1):
+                if pic[i][j]==255:
+                    if pic[i-1][j-1]==0 and pic[i-1][j]==0 and pic[i-1][j+1]==0 and pic[i][j-1]==0 and pic[i][j+1]==0 and pic[i+1][j-1]==0 and pic[i+1][j]==0 and pic[i+1][j+1]==0:
+                        pic[i][j]=0
+        return pic
+
+    def contourClose(selfs,pic):
+        im2, contours, hierarchy = cv2.findContours(pic, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        return cv2.drawContours(pic, contours, -1, (255, 255, 255), 2)
+
+    def morphologicClose(self,pic):
+        kernel = np.ones((3, 3), np.uint8)
+        return cv2.morphologyEx(pic, cv2.MORPH_CLOSE, kernel)
 
     def setFirstStepOutput(self,pic):
         #self.sinogramCanvas.delete("all")
@@ -122,7 +144,7 @@ class Window(Frame):
         return sqrt(suma / (len(pic1)*len(pic1[0])))
 
 root = Tk()
-root.geometry("620x460")
+root.geometry("920x460")
 
 app=Window(root)
 
